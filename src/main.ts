ae01f578309099f6c3d2e33905121644f41c7cf4
feden2906 <feden2906@gmail.com>
2024-02-14 13:58:06 +0200

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('DEMO example')
+    .setDescription('The demo API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      defaultModelsExpandDepth: 3,
+      persistAuthorization: true
+    },
+  });
   await app.listen(3000);
 }
+
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ description: 'field for set user name', default: 'Maksym', required: false })
+  name: string;
+
+  @ApiProperty({ description: 'field for set user email', default: 'maksym@gmail.com' })
+  email: string;
+
+  @ApiProperty()
+  password: string;
+}
