f15bd8d5de5a277ae5b290b692c7bc65c51e5dfd
feden2906 <feden2906@gmail.com>
2024-02-16 23:45:22 +0200

    initial

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -4,23 +4,23 @@ import { UpdateUserDto } from '../dto/req/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
+  public async create(createUserDto: CreateUserDto): Promise<any> {
     return 'This action adds a new user';
   }
 
-  findAll() {
+  public async findAll(): Promise<any> {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
+  public async findOne(id: number): Promise<any> {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  public async update(id: number, updateUserDto: UpdateUserDto): Promise<any> {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  public async remove(id: number): Promise<any> {
     return `This action removes a #${id} user`;
   }
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,18 +1,17 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { CreateUserDto } from './dto/req/create-user.dto';
 import { UpdateUserDto } from './dto/req/update-user.dto';
 import {
   ApiConflictResponse,
-  ApiCreatedResponse,
   ApiForbiddenResponse,
   ApiTags,
   ApiUnprocessableEntityResponse,
@@ -25,30 +24,32 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  @ApiForbiddenResponse({ description: 'Access denied' })
-  @ApiUnprocessableEntityResponse({ description: 'Unprocessable entity' })
-  @ApiConflictResponse({ description: 'Conflict' })
-  create(@Body() createUserDto: CreateUserDto): UserResponseDto {
-    return this.userService.create(createUserDto) as any;
+  public async create(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<UserResponseDto> {
+    return await this.userService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  public async findAll() {
+    return await this.userService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  public async findOne(@Param('id') id: string) {
+    return await this.userService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  public async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return await this.userService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  public async remove(@Param('id') id: string) {
+    return await this.userService.remove(+id);
   }
 }
