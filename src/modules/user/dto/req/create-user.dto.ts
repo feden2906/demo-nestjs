efa6cf686ed1855c0084707826d6a12821465134
feden2906 <feden2906@gmail.com>
2024-02-18 14:59:25 +0200

    initial

--- src/modules/user/dto/req/create-user.dto.ts
+++ src/modules/user/dto/req/create-user.dto.ts
@@ -2,7 +2,7 @@ import { Transform, Type } from 'class-transformer';
 import { IsOptional, IsString, Matches, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @Transform(({value}) => value.trim())
+  @Transform(({ value }) => value.trim())
   @MinLength(3)
   @IsOptional()
   @IsString()
@@ -18,5 +18,5 @@ export class CreateUserDto {
 
   @Type(() => Date)
   @IsOptional()
-  date: string
+  date: string;
 }
